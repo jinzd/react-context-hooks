ab81d4f5d6f2a1e9242661400826475c5558b95c
andy <andytan727@gmail.com>
2020-01-11 23:30:35 +0800

    initial commit

---
+++ hooksapp/src/components/SongList.js
@@ -0,0 +1,26 @@
+import React, { useState } from "react";
+import uuid from "uuid/v1";
+import NewSongForm from "./NewSongForm";
+
+const SongList = () => {
+  const [songs, setSongs] = useState([
+    { title: "almost home", id: 1 },
+    { title: "this wild darkness", id: 2 },
+    { title: "memory gospel", id: 3 }
+  ]);
+  const addSong = title => {
+    setSongs([...songs, { title, id: uuid() }]);
+  };
+  return (
+    <div className="song-list">
+      <ul>
+        {songs.map(song => {
+          return <li key={song.id}>{song.title}</li>;
+        })}
+      </ul>
+      <NewSongForm addSong={addSong} />
+    </div>
+  );
+};
+
+export default SongList;
